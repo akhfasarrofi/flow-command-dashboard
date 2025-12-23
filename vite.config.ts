0b1030064f99b816d8518a1c0791565062eab6fe
dopassss <akhfasarrofi>
2025-12-24 00:00:57 +0700

    remove react-router-devtools from Vite configuration plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { reactRouter } from '@react-router/dev/vite';
 import tailwindcss from '@tailwindcss/vite';
-import { reactRouterDevTools } from 'react-router-devtools';
 import { defineConfig } from 'vite';
 import Inspect from 'vite-plugin-inspect';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -34,24 +33,6 @@ export default defineConfig(({ mode }) => {
     esbuild: {
       pure: ['console.log', 'console.info', 'console.debug'],
     },
-    plugins: [
-      tailwindcss(),
-      reactRouterDevTools({
-        client: {
-          enableInspector: false,
-          showBreakpointIndicator: true,
-        },
-        server: {
-          logs: {
-            actions: true,
-            loaders: false,
-            serverTimings: true,
-          },
-        },
-      }),
-      reactRouter(),
-      tsconfigPaths(),
-      Inspect(),
-    ],
+    plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), Inspect()],
   };
 });
